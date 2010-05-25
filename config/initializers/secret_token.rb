1ac5b9fe31878c735b0b968993f60f5d34cf1093
Jack Dempsey <jack.dempsey@gmail.com>
2010-05-25 14:41:23 -0400

    put a working rails 3 beta3 app together with AAC

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '9871efe1b905725aeba9d5d59727faaf183f79409848319b874970192cfc89e01fb238c7cc270a33e082a931d42761877a35cc019ae6595999cf690a9c9e2c3a'
